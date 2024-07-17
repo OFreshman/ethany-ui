1cb387aeafed0ca92c02f2edbe10e33f634bb665
Ethan <33643257+OFreshman@users.noreply.github.com>
2024-07-17 09:26:42 +0800

    feat: 合并代码

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+// import { defineConfig } from "vite";
+// import vue from "@vitejs/plugin-vue";
+// import vueJSX from "@vitejs/plugin-vue-jsx";
+
+// export default defineConfig({
+//   plugins: [vue(), vueJSX()],
+//   resolve: {
+//     alias: {
+//       "@": "/src",
+//       packages: "/packages",
+//     },
+//   },
+// });
